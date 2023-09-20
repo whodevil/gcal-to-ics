a18b924ac063f685fdf1008d3bb0031bd730c7c0
whodevil <whodevil@offthecob.info>
2023-09-20 12:01:51 -0700

    first pass

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("info.offthecob.Settings") version "1.0.11"
+}
+
+rootProject.name = "gcal-to-org"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from("info.offthecob.platform:catalog:1.0.11")
+            library("offthecob-platform", "info.offthecob.platform:bom:1.0.11")
+        }
+    }
+}
